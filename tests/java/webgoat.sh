3e9612b04915c714410c70ad26115ee65466797a
benjaminyam <ben.yam@gmail.com>
2021-04-07 15:44:13 +0800

    Added cached idir

--- tests/java/webgoat.sh
+++ tests/java/webgoat.sh
@@ -2,6 +2,7 @@ export PATH=/opt/coverity/bin:$(pwd):$PATH
 git clone https://github.com/WebGoat/WebGoat
 cd WebGoat
 git checkout tags/v8.1.0
+export COV_IDIR=/cache/idir/WebGoat
 cov-scan
 # cov-build --dir idir mvn clean compile -DskipTests -Dmaven.test.skip=true
 # cov-capture --dir idir --project-dir .
--- tests/kotlin/tachiyomi.sh
+++ tests/kotlin/tachiyomi.sh
@@ -2,4 +2,5 @@ export PATH=/opt/coverity/bin:$(pwd):$PATH
 git clone https://github.com/tachiyomiorg/tachiyomi
 cd tachiyomi
 git checkout tags/v0.9.2
+export COV_IDIR=/cache/idir/tachiyomi
 cov-scan

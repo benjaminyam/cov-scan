1b0dc9421f4194ff7908dc08c2eb7b078ce443d6
benjaminyam <ben.yam@gmail.com>
2021-04-07 14:19:34 +0800

    Added submit line

--- tests/java/webgoat.sh
+++ tests/java/webgoat.sh
@@ -5,4 +5,5 @@ git checkout tags/v8.1.0
 # cov-scan
 cov-build --dir idir mvn clean compile -DskipTests -Dmaven.test.skip=true
 cov-capture --dir idir --source-dir . --language javascript
-cov-analyze --dir idir --strip-path $(pwd) --all --security --webapp-security --android-security
+cov-analyze --dir idir --strip-path $(pwd) --all --security --webapp-security
+cov-commit-defects --dir idir --url https://ec2-122-248-241-219.ap-southeast-1.compute.amazonaws.com:8443 --on-new-cert trust --stream WebGoat-HEAD

12fe23406593d3ab5129ba50364f495e9f9b506b
benjaminyam <ben.yam@gmail.com>
2021-04-09 21:14:37 +0800

    fixed go test scans

--- tests/golang/hugo.sh
+++ tests/golang/hugo.sh
@@ -1,5 +1,5 @@
 export PATH=/opt/coverity/bin:$(pwd):$PATH
-export COV_IDIR=/cache/idir/consul
-git clone https://github.com/hashicorp/consul
-cd consul
+export COV_IDIR=/cache/idir/hugo
+git clone https://github.com/gohugoio/hugo
+cd hugo
 cov-scan

e192b2ae146c72337237ddc948a935ac5b30572c
benjaminyam <ben.yam@gmail.com>
2021-04-13 10:23:31 +0800

    Added build command

--- tests/golang/vault.sh
+++ tests/golang/vault.sh
@@ -1,8 +1,8 @@
 export PATH=/opt/coverity/bin:$(pwd):$PATH
 export COV_IDIR=/cache/idir/vault
-go get github.com/hashicorp/vault
+git clone https://www.github.com/hashicorp/vault
 mkdir vault
 cd vault
-COV_BUILD_BUILD_CMD="go build -a github.com/hashicorp/vault"
+COV_BUILD_BUILD_CMD="make bootstrap"
 time $COV_BUILD_BUILD_CMD
 cov-scan

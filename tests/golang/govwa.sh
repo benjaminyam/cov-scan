b88c123b46c0c447fc77de3402cbf8e24129fd33
benjaminyam <ben.yam@gmail.com>
2021-04-13 16:45:08 +0800

    clean scan

--- tests/golang/govwa.sh
+++ tests/golang/govwa.sh
@@ -1,5 +1,6 @@
 export PATH=/opt/coverity/bin:$(pwd):$PATH
-#export COV_IDIR=/cache/idir/govwa
+export COV_IDIR=/cache/idir/govwa
+rm $COV_IDIR
 git clone https://github.com/0c34/govwa
 cd govwa
 time go build

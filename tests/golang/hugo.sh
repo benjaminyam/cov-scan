3223b2f8556081ff9466a633bb3bb338e52079df
benjaminyam <ben.yam@gmail.com>
2021-04-14 07:23:27 +0800

    fresh scan (rm idir)

--- tests/golang/hugo.sh
+++ tests/golang/hugo.sh
@@ -1,5 +1,6 @@
 export PATH=/opt/coverity/bin:$(pwd):$PATH
 export COV_IDIR=/cache/idir/hugo
+rm -rf $COV_IDIR
 git clone https://github.com/gohugoio/hugo
 cd hugo
 time go build
--- tests/golang/vault.sh
+++ tests/golang/vault.sh
@@ -1,5 +1,6 @@
 export PATH=/opt/coverity/bin:$(pwd):$PATH
 export COV_IDIR=/cache/idir/vault
+rm -rf $COV_IDIR
 git clone https://www.github.com/hashicorp/vault
 cd vault
 export COV_BUILD_BUILD_CMD="go build -o bin/vault"

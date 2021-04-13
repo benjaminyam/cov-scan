20298765dac2f198c16226d70c77a4ac4c87ff16
benjaminyam <ben.yam@gmail.com>
2021-04-13 14:26:48 +0800

    Updated go builds

--- tests/golang/vault.sh
+++ tests/golang/vault.sh
@@ -1,7 +1,7 @@
 export PATH=/opt/coverity/bin:$(pwd):$PATH
 export COV_IDIR=/cache/idir/vault
 git clone https://www.github.com/hashicorp/vault
-mkdir vault
 cd vault
 COV_BUILD_BUILD_CMD="go build -o bin/vault"
+time $COV_BUILD_BUILD_CMD
 cov-scan
